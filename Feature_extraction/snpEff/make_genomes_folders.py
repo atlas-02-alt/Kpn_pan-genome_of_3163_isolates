65de74c0a2d1d564ee23ceadf3df5a8691433ad4
Officer Micro <sfwang02@outlook.com>
2026-06-13 11:57:59 +0800

    requirements edit

--- Feature_extraction/snpEff/make_genomes_folders.py
+++ Feature_extraction/snpEff/make_genomes_folders.py
@@ -1,6 +1,6 @@
 #Create required folders under `snpEff/data`
 import os
-from config_add import get_file_names_without_extension
+from add_configure import get_file_names_without_extension
 
 folder_path = "./both-align-results-strict-adv/core_gene_sequences_with_ref/"
 
